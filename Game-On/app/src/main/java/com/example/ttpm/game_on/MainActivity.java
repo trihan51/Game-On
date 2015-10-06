934db2e2097c35ecdd9b0269f88a192fee314335
ManbirRandhawa <mrandhawa27@gmail.com>
2015-10-06 11:45:13 -0700

    Register/Login Works

--- Game-On/app/src/main/java/com/example/ttpm/game_on/MainActivity.java
+++ Game-On/app/src/main/java/com/example/ttpm/game_on/MainActivity.java
@@ -24,14 +24,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
     loginbutton = (Button)findViewById(R.id.LoginButton);
         loginbutton.setOnClickListener(new View.OnClickListener() {

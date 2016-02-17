fc2dd7437284d92c5d55ae4aa7d82bfeb18a9198
Toan Tony H. Nguyen <toantonyh.nguyen@gmail.com>
2016-02-17 01:05:17 -0800

    refactor splash screen

--- Game-On/app/src/main/java/com/example/ttpm/game_on/activities/SplashActivity.java
+++ Game-On/app/src/main/java/com/example/ttpm/game_on/activities/SplashActivity.java
@@ -1,77 +1,12 @@
 package com.example.ttpm.game_on.activities;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.support.v4.app.Fragment;
+import com.example.ttpm.game_on.fragments.SplashFragment;
 
-import com.example.ttpm.game_on.R;
-import com.parse.ParseUser;
-
-public class SplashActivity extends AppCompatActivity {
-
-    private Button loginbutton;
-    protected Button registerbutton;
+public class SplashActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        ParseUser currentUser = ParseUser.getCurrentUser();
-        if (currentUser != null) {
-            // do stuff with the user
-            Intent gomainscreen = new Intent(SplashActivity.this, HomeActivity.class);
-            startActivity(gomainscreen);
-        } else {
-            // show the signup or login screen
-        }
-
-        loginbutton = (Button)findViewById(R.id.LoginButton);
-        loginbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent taketologinScreen = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(taketologinScreen);
-                finish();
-            }
-        });
-
-        registerbutton = (Button) findViewById(R.id.RegisterButton);
-        registerbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent taketoRegisterScreen = new Intent(SplashActivity.this, RegisterActivity.class);
-                startActivity(taketoRegisterScreen);
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected Fragment createFragment() {
+        return new SplashFragment();
     }
 }
---
+++ Game-On/app/src/main/java/com/example/ttpm/game_on/fragments/SplashFragment.java
@@ -0,0 +1,80 @@
+package com.example.ttpm.game_on.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.ttpm.game_on.R;
+import com.example.ttpm.game_on.activities.HomeActivity;
+import com.example.ttpm.game_on.activities.LoginActivity;
+import com.example.ttpm.game_on.activities.RegisterActivity;
+import com.parse.ParseUser;
+
+/**
+ * Created by Tony on 2/17/2016.
+ */
+public class SplashFragment extends android.support.v4.app.Fragment{
+
+    private Button mLoginButton;
+    protected Button mRegisterButton;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_splash, container, false);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            Intent gomainscreen = new Intent(getActivity(), HomeActivity.class);
+            startActivity(gomainscreen);
+        }
+
+        mLoginButton = (Button) view.findViewById(R.id.splash_login_button);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mRegisterButton = (Button) view.findViewById(R.id.splash_register_button);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}

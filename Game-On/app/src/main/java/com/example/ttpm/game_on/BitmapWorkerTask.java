d57033122c8dc4881d5e37eff70297b5323d0649
Toan Tony H. Nguyen <toantonyh.nguyen@gmail.com>
2016-03-09 19:49:58 -0800

    load small image with subsmapling

--- Game-On/app/src/main/java/com/example/ttpm/game_on/BitmapWorkerTask.java
+++ Game-On/app/src/main/java/com/example/ttpm/game_on/BitmapWorkerTask.java
@@ -13,6 +13,9 @@ import java.lang.ref.WeakReference;
  */
 public class BitmapWorkerTask extends AsyncTask<File, Void, Bitmap> {
 
+    final static int TARGET_IMAGE_VIEW_HEIGHT = 200;
+    final static int TARGET_IMAGE_VIEW_WIDTH = 200;
+
     WeakReference<ImageView> imageViewReferences;
 
     public BitmapWorkerTask(ImageView imageView) {
@@ -21,7 +24,7 @@ public class BitmapWorkerTask extends AsyncTask<File, Void, Bitmap> {
 
     @Override
     protected Bitmap doInBackground(File... params) {
-        return BitmapFactory.decodeFile(params[0].getAbsolutePath());
+        return decodeBitmapFromFile(params[0]);
     }
 
     @Override
@@ -33,5 +36,32 @@ public class BitmapWorkerTask extends AsyncTask<File, Void, Bitmap> {
                 viewImage.setImageBitmap(bitmap);
             }
         }
+    }
+
+    private int calculateInSampleSize(BitmapFactory.Options bmOptions) {
+        final int photoWidth = bmOptions.outWidth;
+        final int photoHeight = bmOptions.outHeight;
+        int scaleFactor = 1;
+
+        if(photoWidth > TARGET_IMAGE_VIEW_WIDTH || photoHeight > TARGET_IMAGE_VIEW_HEIGHT) {
+            final int halfPhotoWidth = photoWidth/2;
+            final int halfPhotoHeight = photoHeight/2;
+            while(halfPhotoWidth/scaleFactor > TARGET_IMAGE_VIEW_WIDTH
+                    || halfPhotoHeight/scaleFactor > TARGET_IMAGE_VIEW_HEIGHT) {
+                scaleFactor *= 2;
+            }
+        }
+
+        return scaleFactor;
+    }
+
+    private Bitmap decodeBitmapFromFile(File imageFile) {
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFile.getAbsolutePath(), bmOptions);
+        bmOptions.inSampleSize = calculateInSampleSize(bmOptions);
+        bmOptions.inJustDecodeBounds = false;
+        
+        return BitmapFactory.decodeFile(imageFile.getAbsolutePath(), bmOptions);
     }
 }
